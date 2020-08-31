22802ee20a69b1632c09f58661bbfd672a7442c1
sahu74 <hara.sahu@gmail.com>
2020-08-31 20:51:09 +0530

    This File explains about the call () method of Function in js

---
+++ Function_CALL_method.js
@@ -0,0 +1,18 @@
+function Product(n, p) {
+  console.log(" Product Starts \n");
+  this.name = n;
+  this.price = p;
+  console.log(`Name = ${this.name}  and price = ${this.price}  from ${Product.name}`);
+  console.log(" Product ends \n");
+}
+
+function Food(name, price) {
+  console.log(" function Food Starts \n");
+  Product.call(this, name, price);
+  this.category = 'food Category';
+  console.log(" function Food Ends \n");
+}
+var foodObj = new Food('cheese', 5);
+console.log(foodObj);
+//! pring object attributes.
+console.log(foodObj.category);
